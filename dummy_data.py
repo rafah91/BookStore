c8521d8996a2618b9b20a636c6dbabe35d25d98e
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-11-15 22:08:21 +0100

    add dummy Books

--- dummy_data.py
+++ dummy_data.py
@@ -16,16 +16,22 @@ def add_authors(n):
         Author.objects.create(
             name = fake.name() ,
             birth_date=fake.date() ,
-            biography=fake.text() ,
+            biography=fake.text(max_nb_chars=1000) ,
         )
     print(f'{n} authors was created successfully')
 
-#def add_books(n):
-    #fake = Faker()
+def add_books(n):
+    fake = Faker()
+    images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg']
+    for x in range(n):
+        Book.objects.create(
+            title = fake.text(max_nb_chars = 30) ,
+            publication_date= fake.date() ,
+            image= f"bookimages/{images[random.randint(0,6)]}",
+            price= round(random.uniform(19.99,99.99),2) ,
+            author = Author.objects.get(id=random.randint(1,50)),
+        )
+        
+    print(f'{n} books was created successfully')
 
-    #for x in range(n):
-        #Book.objects.create(
-            #n
-        #)
-
-add_authors(50)
+add_books(977)
